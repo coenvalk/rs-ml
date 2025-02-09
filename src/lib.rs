68eaa45ddb67079d9f2cf08ac2c3053663c99563
Coen Valk <clcjvalk@gmail.com>
2025-02-08 22:56:22 -0800

    create traits

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ use std::{
     hash::Hash,
 };
 
-use ndarray::{Array1, Array2, Axis};
+use ndarray::{Array, Array1, Array2, Axis, Dimension};
 use num_traits::Float;
 
 #[derive(Debug)]
@@ -29,16 +29,48 @@ pub struct StandardScaler {
     std_devs: Array1<f64>,
 }
 
-impl StandardScaler {
-    pub fn fit(arr: &Array2<f64>) -> Self {
-        StandardScaler {
-            means: arr.mean_axis(Axis(0)).unwrap(),
-            std_devs: arr.std_axis(Axis(0), (arr.shape()[0] - 1) as f64),
-        }
+pub fn train_test_split<D: Dimension, D2: Dimension<Larger = D>>(
+    arr: &Array2<f64>,
+    split: f64,
+) -> (&Array<f64, D>, &Array<f64, D>) {
+    let nrows = arr.shape()[0];
+    let v = arr.rows();
+
+    todo!()
+}
+
+trait Transformer {
+    type Input;
+    type Output;
+
+    fn fit(input: &Self::Input) -> Option<Self>
+    where
+        Self: Sized;
+
+    fn transform(&self, input: &Self::Input) -> Option<Self::Output>;
+
+    fn fit_transform(input: &Self::Input) -> Option<Self::Output>
+    where
+        Self: Sized,
+    {
+        let transformer = Self::fit(input)?;
+        transformer.transform(input)
+    }
+}
+
+impl Transformer for StandardScaler {
+    type Input = Array2<f64>;
+    type Output = Array2<f64>;
+
+    fn fit(input: &Array2<f64>) -> Option<Self> {
+        Some(StandardScaler {
+            means: input.mean_axis(Axis(0))?,
+            std_devs: input.std_axis(Axis(0), (input.shape()[0] - 1) as f64),
+        })
     }
 
-    pub fn transform(&self, arr: &Array2<f64>) -> Array2<f64> {
-        (arr - &self.means) / &self.std_devs
+    fn transform(&self, arr: &Array2<f64>) -> Option<Array2<f64>> {
+        Some((arr - &self.means) / &self.std_devs)
     }
 }
 
@@ -81,11 +113,9 @@ impl<Label: Hash + Eq + Clone> GaussianNB<Label> {
         let root_2pi = f64::sqrt(2. * f64::consts::PI);
         for (label, data) in self.data.iter() {
             let p1 = -(arr - &data.mean).pow2() / (2. * &data.std_dev.pow2());
-
             let p2 = (&data.std_dev * root_2pi).recip();
 
-            let p = (p2 * p1.exp() / data.posterior).product_axis(Axis(1));
-
+            let p = (p2 * p1.exp()).product_axis(Axis(1)) * data.posterior;
             likelihood.insert(label.clone(), p);
         }
 
@@ -125,7 +155,7 @@ impl<F: Float> MinMaxScaler<F> {
 
 #[cfg(test)]
 mod tests {
-    use ndarray::arr2;
+    use ndarray::{arr2, Array};
     use rand::{rng, seq::SliceRandom};
     use serde::{Deserialize, Serialize};
 
@@ -166,9 +196,9 @@ mod tests {
             [8., 24., 100.],
         ]);
 
-        let scaler = StandardScaler::fit(&arr);
+        let scaler = StandardScaler::fit(&arr).unwrap();
 
-        let scaled = scaler.transform(&arr);
+        let scaled = scaler.transform(&arr).unwrap();
 
         println!("{}", scaled);
 
@@ -200,16 +230,16 @@ mod tests {
             labels.push(data.species.clone());
         }
 
-        let transformer = StandardScaler::fit(&arr);
-        let scaled = transformer.transform(&arr);
+        let transformer = StandardScaler::fit(&arr).unwrap();
+        let scaled = transformer.transform(&arr).unwrap();
 
-        let model = GaussianNB::fit(&arr, &labels);
-
-        let predictions = model.predict(&arr);
+        let model = GaussianNB::fit(&scaled, &labels);
+        let inference = model.predict(&scaled);
 
         for i in 0..labels.len() {
             println!("gt: {}", labels[i]);
-            for (label, row) in predictions.iter() {
+
+            for (label, row) in inference.iter() {
                 println!("likelihood of {}: {:.4}", label, row[i]);
             }
 

80070788b33ce43477f6244a0f42483faf558591
Coen Valk <clcjvalk@gmail.com>
2025-02-09 12:36:31 -0800

    move iris integration test to own file

--- tests/tests.rs
+++ tests/tests.rs
@@ -6,15 +6,6 @@ use rs_ml::transformer::Transformer;
 use rs_ml::GaussianNB;
 use serde::{Deserialize, Serialize};
 
-#[derive(Deserialize, Serialize)]
-struct DataPoint {
-    sepal_length: f64,
-    sepal_width: f64,
-    petal_length: f64,
-    petal_width: f64,
-    species: String,
-}
-
 #[test]
 fn it_works() {
     let arr = arr2(&[
@@ -49,44 +40,4 @@ fn standard_scaler() {
 
     println!("means: {}", scaled.view().mean_axis(Axis(0)).unwrap());
     println!("std: {}", scaled.view().std_axis(Axis(0), 4.));
-}
-
-#[test]
-fn iris() {
-    let mut csv = csv::Reader::from_path("iris.csv").unwrap();
-
-    let mut a: Vec<DataPoint> = csv
-        .deserialize::<DataPoint>()
-        .filter_map(|r| r.ok())
-        .collect();
-
-    a.shuffle(&mut rng());
-
-    let mut iris = Array::zeros((a.len(), 4));
-
-    let mut labels = vec![];
-
-    for (idx, data) in a.iter().enumerate() {
-        iris[[idx, 0]] = data.sepal_length;
-        iris[[idx, 1]] = data.sepal_width;
-        iris[[idx, 2]] = data.petal_length;
-        iris[[idx, 3]] = data.petal_width;
-
-        labels.push(data.species.clone());
-    }
-
-    let scaled = StandardScaler::fit_transform(&iris).unwrap();
-
-    let model = GaussianNB::fit(&scaled, &labels);
-    let inference = model.predict(&scaled);
-
-    for i in 0..labels.len() {
-        println!("gt: {}", labels[i]);
-
-        for (label, row) in inference.iter() {
-            println!("likelihood of {}: {:.4}", label, row[i]);
-        }
-
-        println!();
-    }
 }
